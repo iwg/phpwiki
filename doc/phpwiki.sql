78bc145f27f9be543f2911b635f774e4155df019
Xiao Jia <stfairy@gmail.com>
2012-02-14 09:37:01 +0800

    add new model: Preview

--- doc/phpwiki.sql
+++ doc/phpwiki.sql
@@ -1,24 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
-
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
-
---
--- Database: `phpwiki`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `groups`
---
 
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -27,21 +12,11 @@ CREATE TABLE IF NOT EXISTS `groups` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `groups`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 INSERT INTO `groups` (`id`, `name`, `created_at`, `updated_at`) VALUES
-(1, 'root', '0000-00-00 00:00:00', '2012-02-08 08:29:43'),
-(2, 'nobody', '0000-00-00 00:00:00', '2012-02-08 08:29:43');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hyperlinks`
---
+(1, 'root', '0000-00-00 00:00:00', '0000-00-00 00:00:00'),
+(2, 'nobody', '0000-00-00 00:00:00', '0000-00-00 00:00:00');
 
 CREATE TABLE IF NOT EXISTS `hyperlinks` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -54,17 +29,6 @@ CREATE TABLE IF NOT EXISTS `hyperlinks` (
   KEY `page_id` (`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `hyperlinks`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `memberships`
---
-
 CREATE TABLE IF NOT EXISTS `memberships` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `group_id` bigint(20) unsigned NOT NULL,
@@ -74,18 +38,7 @@ CREATE TABLE IF NOT EXISTS `memberships` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `group_id_2` (`group_id`,`user_name`),
   KEY `group_id` (`group_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `memberships`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `pages`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=40 ;
 
 CREATE TABLE IF NOT EXISTS `pages` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -100,18 +53,24 @@ CREATE TABLE IF NOT EXISTS `pages` (
   UNIQUE KEY `path` (`path`),
   KEY `group_id` (`group_id`),
   KEY `owner_name` (`owner_name`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
+
+CREATE TABLE IF NOT EXISTS `previews` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `path` varchar(255) NOT NULL,
+  `owner_name` varchar(255) NOT NULL,
+  `group_id` bigint(20) unsigned NOT NULL,
+  `permission` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `body` text NOT NULL,
+  `markup_name` varchar(255) NOT NULL,
+  `theme_id` bigint(20) unsigned NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `group_id` (`group_id`),
+  KEY `theme_id` (`theme_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `pages`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `revisions`
---
 
 CREATE TABLE IF NOT EXISTS `revisions` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -128,18 +87,7 @@ CREATE TABLE IF NOT EXISTS `revisions` (
   PRIMARY KEY (`id`),
   KEY `page_id` (`page_id`),
   KEY `theme_id` (`theme_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
---
--- Dumping data for table `revisions`
---
-
-
--- --------------------------------------------------------
-
---
--- Table structure for table `themes`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 CREATE TABLE IF NOT EXISTS `themes` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
@@ -148,38 +96,21 @@ CREATE TABLE IF NOT EXISTS `themes` (
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
---
--- Dumping data for table `themes`
---
-
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `hyperlinks`
---
 ALTER TABLE `hyperlinks`
   ADD CONSTRAINT `hyperlinks_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `memberships`
---
 ALTER TABLE `memberships`
   ADD CONSTRAINT `memberships_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE;
 
---
--- Constraints for table `pages`
---
 ALTER TABLE `pages`
   ADD CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`);
 
---
--- Constraints for table `revisions`
---
+ALTER TABLE `previews`
+  ADD CONSTRAINT `previews_ibfk_2` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `previews_ibfk_1` FOREIGN KEY (`group_id`) REFERENCES `groups` (`id`) ON DELETE CASCADE;
+
 ALTER TABLE `revisions`
-  ADD CONSTRAINT `revisions_ibfk_2` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`id`),
-  ADD CONSTRAINT `revisions_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `revisions_ibfk_1` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `revisions_ibfk_2` FOREIGN KEY (`theme_id`) REFERENCES `themes` (`id`);

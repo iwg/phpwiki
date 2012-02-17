1d8984986c1be2aae858e458cce5f21013daccf1
Xiao Jia <stfairy@gmail.com>
2012-02-17 15:58:57 +0800

    buggy lists

--- inc/plugins/mediawiki.php
+++ inc/plugins/mediawiki.php
@@ -30,8 +30,7 @@ function wiki_render_markup($title, $text)
 	$html = preg_replace('/\n\n+/', "\n<br/>\n", $html);
 	
 	// lists
-	$html = preg_replace('/((^\s*[*#]+[^\n]*$\n)+)/m', '<pre>${1}</pre>', $html);
-	// TODO change them to real lists
+	$html = preg_replace_callback('/((^[*#\s]+[^\n]*$\n)+)/m', 'wiki_render_lists', $html);
 	
 	// horizontal rule
 	$html = preg_replace('/----/', '<hr/>', $html);
@@ -55,4 +54,81 @@ function wiki_render_internal_link_without_text($matches)
   $path = $matches[1];
   $target = SITE_BASE.'/'.$path;
   return '<a href="'.$target.'">'.$path.'</a>';
+}
+
+function wiki_preprocess_lists_lines($lines)
+{
+  foreach ($lines as $i => $line) {
+    $lines[$i] = trim($line);
+  }
+  return array_filter($lines, 'strlen');
+}
+
+function wiki_segment_lists_lines($lines)
+{
+  $segments = array();
+  if (empty($lines)) {
+    return $segments;
+  }
+  $char = $lines[0][0];
+  $segment = array();
+  foreach ($lines as $line) {
+    if ($line[0] == $char) {
+      $segment[] = $line;
+    } else {
+      $segments[] = $segment;
+      $char = $line[0];
+      $segment = array($line);
+    }
+  }
+  $segments[] = $segment;
+  return $segments;
+}
+
+function wiki_render_lists($matches)
+{
+  $text = $matches[1];
+  $lines = explode("\n", trim($text));
+  $lines = wiki_preprocess_lists_lines($lines);
+  return wiki_render_lists_lines($lines);
+}
+
+function wiki_render_lists_lines($lines)
+{  
+  $segments = wiki_segment_lists_lines($lines);
+  $html = "";
+  foreach ($segments as $segment) {
+    $html .= wiki_render_list($segment);
+  }
+  return $html;
+}
+
+function wiki_render_list($lines)
+{
+  $char = $lines[0][0];
+  foreach ($lines as $i => $line) {
+    $lines[$i] = trim(substr($line, 1));
+  }
+  $html = "";
+  $i = 0;
+  while ($i < count($lines)) {
+    if ($lines[$i][0] == '*' or $lines[$i][0] == '#') {
+      $sublines = array();
+      $j = $i;
+      while ($j < count($lines) and ($lines[$j][0] == '*' or $lines[$j][0] == '#')) {
+        $sublines[] = $lines[$j];
+        $j++;
+      }
+      $html .= '<li>'.wiki_render_lists_lines($sublines).'</li>';
+      $i = $j;
+    } else {
+      $html .= '<li>'.$lines[$i].'</li>';
+      $i++;
+    }
+  }
+  if ($char == '*') {
+    return '<ul>'.$html.'</ul>';
+  } else {
+    return '<ol>'.$html.'</ol>';
+  }
 }

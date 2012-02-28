2e91a7fc4b405d528520e8b1b24e72449b4566a5
lgr150 <root@ubuntu.ubuntu-domain>
2012-02-28 22:21:06 +0800

    add mysql-table locks

--- inc/config-example.php
+++ inc/config-example.php
@@ -10,3 +10,5 @@ define('THEME_BASE', '/wiki/themes');
 define('DEFAULT_THEME', 'default');
 define('TITLE_SUFFIX', ' | phpwiki');
 define('LOCK_TIME', 100);
+
+date_default_timezone_set("Asia/Chongqing"); 

4218eb17bdefad52822929271cc9732200483a17
Xiao Jia <stfairy@gmail.com>
2012-02-29 07:12:27 +0800

    fix default timezone

--- inc/config-example.php
+++ inc/config-example.php
@@ -11,4 +11,5 @@ define('DEFAULT_THEME', 'default');
 define('TITLE_SUFFIX', ' | phpwiki');
 define('LOCK_TIME', 100);
 
-date_default_timezone_set("Asia/Chongqing"); 
+date_default_timezone_set("Asia/Shanghai");
+

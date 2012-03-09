79571de5f4d17a7373eb6b2d62b019f04270847a
LGR150 <lgr150@ubuntu.ubuntu-domain>
2012-03-09 15:38:18 +0800

    config-example...

--- inc/config-example.php
+++ inc/config-example.php
@@ -10,9 +10,9 @@ define('THEME_BASE', '/wiki/themes');
 define('DEFAULT_THEME', 'default');
 define('TITLE_SUFFIX', ' | phpwiki');
 define('LOCK_TIME', 100);
-define('NOBODY_GROUP', 2);
 
 date_default_timezone_set("Asia/Shanghai");
 
-$REALROOT_IDS[1] = 1;
+//root_ids ---- which have the permission to change groups & themes
+$ROOT_IDS[1] = 1;
 

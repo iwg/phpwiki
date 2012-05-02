6c1906109b9c93bf350e8fa6738646be4a7a32c0
LGR150 <lgr150@ubuntu.ubuntu-domain>
2012-05-02 21:13:37 +0800

    fix a bug about homepage

--- create-link.php
+++ create-link.php
@@ -12,16 +12,14 @@ if (fRequest::isPost()) {
     }
 
     $parent_path = Page::parentPage($page_path);
-    if ($parent_path != '/') {
-      try {
-        $parent = new Page(array('path' => $parent_path));
-      } catch (fNotFoundException $e) {
-        throw new fValidationException('Please create the parent page first');
-      }
-      $user_name = wiki_get_current_user();
-      if (!$parent->isPermitted($user_name, 'create')) {
-        throw new fValidationException('You are not permitted to create links here!');
-      }
+    try {
+      $parent = new Page(array('path' => $parent_path));
+    } catch (fNotFoundException $e) {
+      throw new fValidationException('Please create the parent page first');
+    }
+    $user_name = wiki_get_current_user();
+    if (!$parent->isPermitted($user_name, 'create')) {
+      throw new fValidationException('You are not permitted to create links here!');
     }
 
     $dest = trim(fRequest::get('dest'));
